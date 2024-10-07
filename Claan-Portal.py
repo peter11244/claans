7cb14f91e542ddf0b944c2019d3dca8f732d81e2
Peter Sach <1846224+peter11244@users.noreply.github.com>
2024-10-07 15:10:19 +0000

    msal test 1

--- Claan-Portal.py
+++ Claan-Portal.py
@@ -1,6 +1,7 @@
 import pathlib
 
 import streamlit as st
+from streamlit_msal import Msal
 
 from src.models.claan import Claan
 from src.utils import data
@@ -18,6 +19,24 @@ def init_page() -> None:
         </style>""",
         unsafe_allow_html=True,
     )
+
+    with st.sidebar:
+        auth_data = Msal.initialize_ui(
+            client_id="866dd59e-3ab4-41af-91fe-510d7ad9113e",
+            authority="https://login.microsoftonline.com/6d2c78dd-1f85-4ccb-9ae3-cd5ea1cca361",
+            scopes=[], # Optional
+            # Customize (Default values):
+            connecting_label="Connecting",
+            disconnected_label="Disconnected",
+            sign_in_label="Sign in",
+            sign_out_label="Sign out"
+        )
+
+    if not auth_data:
+        st.write("Authenticate to access protected content")
+        st.stop()
+
+    st.write("Protected content available")
 
     with Database.get_session() as session:
         if "scores" not in st.session_state:
