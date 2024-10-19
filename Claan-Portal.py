38963ce61101d4f52ef950d37f75098c1e0b7281
Peter Sach <1846224+peter11244@users.noreply.github.com>
2024-10-19 04:55:47 +0000

    login

--- Claan-Portal.py
+++ Claan-Portal.py
@@ -1,23 +1,33 @@
 import streamlit as st
 from menu import menu
+from streamlit_msal import Msal
+from src.utils.data.users import get_current_user
+from src.utils.database import Database
 
-# Initialize st.session_state.role to None
-if "role" not in st.session_state:
-    st.session_state.role = None
+auth_data = Msal.initialize_ui(
+        client_id="866dd59e-3ab4-41af-91fe-510d7ad9113e",
+        authority="https://login.microsoftonline.com/6d2c78dd-1f85-4ccb-9ae3-cd5ea1cca361",
+        scopes=[], # Optional
+        # Customize (Default values):
+        connecting_label="Connecting",
+        disconnected_label="Disconnected",
+        sign_in_label="Sign in",
+        sign_out_label="Sign out"
+    )
 
-# Retrieve the role from Session State to initialize the widget
-st.session_state._role = st.session_state.role
+if not auth_data:
+    st.write("Welcome to Claans! Please sign in to continue.")
+    st.stop()
 
-def set_role():
-    # Callback function to save the role selection to Session State
-    st.session_state.role = st.session_state._role
+else:
+    # name = auth_data["account"]["name"]
+    email = auth_data["account"]["username"]
+    with Database.get_session() as session:
+        if "current_user" not in st.session_state:
+            # TODO: Ensure single user?
+            st.session_state["current_user"] = get_current_user(session, email)[0]
 
 
-# Selectbox to choose role
-st.selectbox(
-    "Select your role:",
-    [None, "user", "admin", "super-admin"],
-    key="_role",
-    on_change=set_role,
-)
+
+
 menu() # Render the dynamic menu!
--- menu.py
+++ menu.py
@@ -3,8 +3,7 @@ import streamlit as st
 
 def authenticated_menu():
     # Show a navigation menu for authenticated users
-    st.sidebar.page_link("Claan-Portal.py", label="Switch accounts")
-    st.sidebar.page_link("pages/1_Earth_Striders.py", label="Your profile")
+    st.sidebar.page_link("pages/0_Home.py", label="Claans Home")
     if st.session_state.role in ["admin", "super-admin"]:
         st.sidebar.page_link("pages/7_Admin.py", label="Manage users")
 
@@ -26,6 +25,6 @@ def menu():
 def menu_with_redirect():
     # Redirect users to the main page if not logged in, otherwise continue to
     # render the navigation menu
-    if "role" not in st.session_state or st.session_state.role is None:
+    if "current_user" not in st.session_state:
         st.switch_page("Claan-Portal.py")
     menu()
