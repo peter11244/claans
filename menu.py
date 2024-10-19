9e8b7fb18628e8e0f366e6ce18ecd9ca7229caa0
Peter Sach <1846224+peter11244@users.noreply.github.com>
2024-10-19 05:07:09 +0000

    Claans

--- menu.py
+++ menu.py
@@ -4,6 +4,20 @@ import streamlit as st
 def authenticated_menu():
     # Show a navigation menu for authenticated users
     st.sidebar.page_link("pages/0_Home.py", label="Claans Home")
+    # Clean Up
+    match st.session_state["current_user"].claan.value:
+        case "Earth Striders":
+            st.sidebar.page_link("pages/1_Earth_Striders.py", label="Earth Striders")
+        case "Fire Dancers":
+            st.sidebar.page_link("pages/2_Fire_Dancers.py", label="Fire Dancers")
+        case "Thunder Walkers":
+            st.sidebar.page_link("pages/3_Thunder_Walkers.py", label="Thunder Walkers")
+        case "Wave Riders":
+            st.sidebar.page_link("pages/4_Wave_Riders.py", label="Wave Riders")
+        case "Beast Runners":
+            st.sidebar.page_link("pages/5_Beast_Runners.py", label="Beast Runners")
+        case "Iron Stalkers":
+            st.sidebar.page_link("pages/6_Iron_Stalkers.py", label="Iron Stalkers")
     if st.session_state["current_user"].email == "peter.sach@advancinganalytics.co.uk":
         st.sidebar.page_link("pages/7_Admin.py", label="Manage users")
 
