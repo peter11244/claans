aed24cbb5c0ef292eeab6155f2d48661f299ad34
Peter Sach <1846224+peter11244@users.noreply.github.com>
2024-10-19 05:48:15 +0000

    remove password

--- src/utils/claan_page.py
+++ src/utils/claan_page.py
@@ -97,36 +97,7 @@ class ClaanPage:
 
         self.build_page()
 
-    def check_password(self) -> bool:
-        def password_entered():
-            if st.session_state["password"] == st.secrets["passwords"][self.claan.name]:
-                st.session_state[f"{self.claan.name}_password_correct"] = True
-                del st.session_state["password"]
-            else:
-                st.session_state[f"{self.claan.name}_password_correct"] = False
-
-        if st.secrets.env.get("debug"):
-            return True
-
-        if f"{self.claan.name}_password_correct" not in st.session_state:
-            st.text_input(
-                "Password", type="password", on_change=password_entered, key="password"
-            )
-            return False
-
-        elif not st.session_state[f"{self.claan.name}_password_correct"]:
-            st.text_input(
-                "Password", type="password", on_change=password_entered, key="password"
-            )
-            st.error("😕 Password incorrect")
-            return False
-
-        else:
-            return True
-
     def build_page(self):
-        if not self.check_password():
-            return
 
         # =-- HEADER --= #
         with st.container():
