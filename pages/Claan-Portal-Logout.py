92943b9d61db81e90ac6df43f1b4cbc8857aef52
Peter Sach <1846224+peter11244@users.noreply.github.com>
2024-10-19 10:26:56 +0000

    ui

--- pages/Claan-Portal-Logout.py
+++ pages/Claan-Portal-Logout.py
@@ -4,16 +4,18 @@ from streamlit_msal import Msal
 from src.utils.data.users import get_current_user
 from src.utils.database import Database
 
+
+
 auth_data = Msal.initialize_ui(
-        client_id="866dd59e-3ab4-41af-91fe-510d7ad9113e",
-        authority="https://login.microsoftonline.com/6d2c78dd-1f85-4ccb-9ae3-cd5ea1cca361",
-        scopes=[], # Optional
-        # Customize (Default values):
-        connecting_label="Connecting",
-        disconnected_label="Disconnected",
-        sign_in_label="Sign in",
-        sign_out_label="Sign out"
-    )
+    client_id="866dd59e-3ab4-41af-91fe-510d7ad9113e",
+    authority="https://login.microsoftonline.com/6d2c78dd-1f85-4ccb-9ae3-cd5ea1cca361",
+    scopes=[], # Optional
+    # Customize (Default values):
+    connecting_label="Connecting",
+    disconnected_label="Disconnected",
+    sign_in_label="Sign in",
+    sign_out_label="Sign out"
+)
 
 if not auth_data:
     st.session_state.pop("current_user", None)
